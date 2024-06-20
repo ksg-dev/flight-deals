a42ca097b7299a7476b3e26fb07d0ea84c7948e0
ksg-dev <ksg.dev.data@gmail.com>
2024-06-20 14:58:40 -0500

    working out flight data dict

--- flight_data.py
+++ flight_data.py
@@ -1,3 +1,32 @@
+# This class is responsible for structuring the flight data.
+import requests
+from datetime import datetime
+
+
+FLIGHT_OFFERS_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    def __init__(self):
+        self.price = 0
+        self.departure_code = "LON"
+
+    def get_flights(self):
+        today = datetime.now()
+
+
+        header = {
+            "X-HTTP-Method-Override": "GET",
+        }
+
+        o_destinations = {
+            "originLocationCode": self.departure_code,
+            "destinationLocationCode": "DUB",
+            "departureDateTimeRange": {
+                "date":
+            }
+        }
+
+        parameters = {
+            "currencyCode": "GBP",
+            "originDestinations":
+        }
