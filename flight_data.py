f3d59b7adbc7dc6364060badaa19e68dc7bcc152
ksg-dev <ksg.dev.data@gmail.com>
2024-06-20 15:27:17 -0500

    flight data w post request before rework

--- flight_data.py
+++ flight_data.py
@@ -1,6 +1,7 @@
 # This class is responsible for structuring the flight data.
 import requests
-from datetime import datetime
+from datetime import date
+from flight_search import FlightSearch
 
 
 FLIGHT_OFFERS_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
@@ -9,24 +10,38 @@ class FlightData:
     def __init__(self):
         self.price = 0
         self.departure_code = "LON"
+        search = FlightSearch()
+        self._token = search._get_new_token()
 
-    def get_flights(self):
-        today = datetime.now()
-
+    def get_flights(self, destination_iata):
+        today = date.today()
+        tom = today.replace(day=today.day + 1)
+        tomorrow = tom.isoformat()
 
         header = {
             "X-HTTP-Method-Override": "GET",
+            "Authorization": f"Bearer {self._token}"
         }
 
         o_destinations = {
             "originLocationCode": self.departure_code,
-            "destinationLocationCode": "DUB",
+            "destinationLocationCode": destination_iata,
             "departureDateTimeRange": {
-                "date":
+                "date": tomorrow,
             }
         }
 
-        parameters = {
-            "currencyCode": "GBP",
-            "originDestinations":
-        }
+        body = {
+            "currencyCode": "USD",
+            "originDestinations": o_destinations,
+            "travelers": {
+                "id": "1",
+                "travelerType": "ADULT"
+                },
+            "sources": "GDS"
+            }
+
+        response = requests.post(url=FLIGHT_OFFERS_ENDPOINT, headers=header, data=body)
+        print(response.status_code)
+        data = response.json()
+        print(data)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import FlightData
 from pprint import pprint
 from dotenv import load_dotenv
 import os
@@ -15,12 +16,14 @@ sheet_data = sheet.get_data()
 search = FlightSearch()
 
 # Check if row has value in IATA code, if not, update w flight search
-for row in sheet_data:
-    if row["iataCode"] == "":
-        row["iataCode"] = search.get_iata(row["city"])
-sheet.destination_data = sheet_data
-sheet.update_data()
+# for row in sheet_data:
+#     if row["iataCode"] == "":
+#         row["iataCode"] = search.get_iata(row["city"])
+# sheet.destination_data = sheet_data
+# sheet.update_data()
 
+fl_data = FlightData()
+fl_data.get_flights("NYC")
 
 
 
